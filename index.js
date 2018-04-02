23ef0a46ffff0ee34f75e883baae2b2353103fbe
= <=>
2018-04-02 07:05:45 +0000

    Add config for mac addresses and servos

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const { MacScanner } = require("mac-scanner")
 const Servo = require('./servo')
+const ServoConfig = require('./servo-config')
 
-const servo0 = new Servo(0, (status) => {
-  if (status.success) {
-    console.log('servo0 activated')
+const config = {
+  debug: false,
+  initial: false, //if false omits initial entering of all available hosts in the network
+  network: "192.168.0.1/24",
+  concurrency: 50, //amount of ips that are pinged in parallel
+  scanTimeout: 5000
+}
+const scanner = new MacScanner(config)
+scanner.start()
 
-    const config = {
-      debug: false,
-      initial: false, //if false omits initial entering of all available hosts in the network
-      network: "192.168.0.1/24",
-      concurrency: 50, //amount of ips that are pinged in parallel
-      scanTimeout: 5000
+ServoConfig.forEach((servoInfo, index) => {
+  const servo = new Servo(index, (status) => {
+    if (status.success) {
+      console.log(`servo ${index} activated`)
+  
+      scanner.on("entered", target => {
+        if (target.mac === servoInfo.mac) {
+          servo.setAngle(180)
+          console.log(`servo ${index} entered`, target.ip, target.mac)
+        }
+      })
+  
+      scanner.on("left", target => {
+        if (target.mac === servoInfo.mac) {
+          servo.setAngle(0)
+          console.log(`servo ${index} left`, target.ip, target.mac)
+        }
+      })
+  
+    } else {
+      console.log('error is:', status.error)
     }
-
-    const scanner = new MacScanner(config)
-    scanner.start()
-
-    scanner.on("entered", target => {
-      if (target.mac === '') {
-        servo0.setAngle(180)
-      }
-      console.log("he is here", target.ip, target.mac)
-    })
-
-    scanner.on("left", target => {
-      if (target.mac === '') {
-        servo0.setAngle(0)
-      }
-      console.log('off network', target.ip, target.mac)
-    })
-
-  } else {
-    console.log('error is:', status.error)
-  }
+  })
 })
